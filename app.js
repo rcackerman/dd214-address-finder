c9df69de07023610def178d9aa12609eb66d2bdd
Rebecca Ackerman <rcackerman@gmail.com>
2018-03-30 12:17:16 -0400

    Calculating discharge date and returning where to send form

--- app.js
+++ app.js
@@ -161,19 +161,91 @@ var populateStatusDropdown = function(branch) {
 
 var clearResults = function() {
   $("#results").children().hide();
+  $("#finder").removeData("personnel").removeData("medserve");
   $("#discharge-date").hide();
   $("#discharge-date").find("input").val("");
   return true;
 }
 
-var getRetirementDate = function(dateString) {
-  return true;
+var getRetirementDischargeDate = function(formSection) {
+  let m = formSection.find("#d-month").val();
+  let d = formSection.find("#d-day").val();
+  let y = formSection.find("#d-year").val();
+  return new Date(y, m, d);
 }
 
-var getWhereToSend = function(destinationDict) {
+var getWhereToSend = function(branch, dDate) {
+  if (branch == 'airForce') {
+    // Air Force, discharged, before 1994 (row 1 in AF)
+    if ( dDate < (new Date('5/1/1994')) ) {
+      return {personnel: 14, medserv: 14}
+    // Air Force, discharged, between 5/1/94 - 9/30/04 (row 2 in AF)
+    } else if ( (dDate >= (new Date('5/1/1994')))
+                && ( dDate <= (new Date('9/30/2004')) ) ){
+      return {personnel: 14, medserv: 11}
+    // Air Force, discharged, between 10/1/04 - 12/31/13 (row 3 in AF)
+    } else if ( (dDate >= (new Date('10/1/2004')))
+                && (dDate <= (new Date('12/31/2013'))) ) {
+      return {personnel: 1, medserv: 11}
+    // Air Force, discharged, after 1/1/2014 (row 4 in AF)
+    } else if ( dDate >= (new Date('1/1/2014')) ) {
+      return {personnel: 1, medserv: 13}
+    }
+  } else if (branch == 'coastGuard') {
+    if ( dDate <= (new Date('3/31/1998')) ) {
+      return { personnel: 14, medserv: 14 }
+    } else if ( (dDate >= (new Date('4/1/1998')))
+                && (dDate <= new Date('9/30/2006')) ) {
+      return { personnel: 14, medserv: 11}
+    } else if ( (dDate >= (new Date('10/1/2006')))
+                && (dDate <= (new Date('9/30/2013'))) ) {
+      return { personnel: 3, medserv: 11 }
+    } else if ( dDate >= (new Date('10/1/2013')) )  {
+      return { personnel: 3, medserv: 14}
+    }
+  } else if ( branch == 'marineCorps' ) {
+    if ( dDate <= (new Date('4/30/1994')) ) {
+      return { personnel: 14, medserv: 14 }
+    } else if ( (dDate >= (new Date('5/1/1994')))
+               && (dDate <= (new Date('12/31/1998'))) ) {
+      return { personnel: 14, medserv: 11 }
+    } else if ( (dDate >= (new Date('1/1/1999')))
+                && (dDate <= (new Date('12/31/2013'))) ) {
+      return { personnel: 4, medserv: 11 }
+    } else {
+      return { personnel: 4, medserv: 8 }
+    }
+  } else if ( branch == 'army' ) {
+    if ( dDate >= (new Date('10/15/1992')) ) {
+      return { personnel: 14 }
+    } else if ( (dDate >= (new Date('10/16/1992')))
+                && (dDate <= (new Date('9/30/2002'))) ) {
+      return { personnel: 14, medserv: 11 }
+    } else if ( (dDate >= (new Date('10/1/2002')))
+                && (dDate <= (new Date('12/31/2013'))) ) {
+      return { personnel: 7, medserv: 11 }
+    } else {
+      return { personnel: 7, medserv: 9 }
+    }
+  } else if ( branch == 'navy' ) {
+    if ( dDate >= (new Date('1/30/1994'))) {
+      return { personnel: 14, medserv: 14 }
+    } else if ( (dDate >= (new Date('1/31/1994')))
+                && (dDate <= (new Date('12/31/1994'))) ) {
+      return { personnel: 14, medserv: 11 }
+    } else if ( (dDate >= (new Date('1/1/1995')))
+                && (dDate <= (new Date('12/31/2013'))) ) {
+      return { personnel: 10, medserv: 11 }
+    } else {
+      return { personnel: 10, medserv: 8 }
+    }
+  } 
+}
+
+var showWhereToSend = function(destinationDict) {
   // Find the personnel and medical/service treatment location
   // using data from the #current-status-select option chosen
-  $.each(destinationDict, function(k,v){
+  $.each(destinationDict, function(k,v) {
     $("#results").show();
     $("#address" + v).show();
   })
@@ -199,16 +271,22 @@ $( document ).ready(function(){
       // Keep track of where to send the request by adding data to the form
       var destinations = $( this ).children( 'option:selected' ).data();
       $("#finder").data(destinations);
-      // Show the submit button at this point
-      $("button").show();
     } else {
+      // Show the discharge date section
       $("#discharge-date").show();
     }
+    // Show the submit button
+    $("button").show();
   });
 
-  $("#finder").on("submit", function(event){
-      getWhereToSend($(this).data());
-      event.preventDefault();
+  $("#finder").on("submit", function( event ){
+    if ( $(this).data('personnel') == undefined ) {
+      let date = getRetirementDischargeDate($("#discharge-date"));
+      let branch = $("#finder").data('branch');
+      $("#finder").data(getWhereToSend(branch, date));
+    }
+    showWhereToSend($(this).data());
+    event.preventDefault();
   });
 
 });
